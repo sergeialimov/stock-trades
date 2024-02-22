b77fc81d2bfc85d4fc9c8767ee5c202acbaa8c42
Sergei <dev.sergeialimov@gmail.com>
2024-02-22 18:30:16 +0400

    feat: add trades model

--- models/trades.js
+++ models/trades.js
@@ -1,9 +1,54 @@
-// Uncomment the code below to use Sequelize ORM
-// const {Sequelize} = require("sequelize");
-// const sequelize = new Sequelize("sqlite::memory:");
+const { DataTypes, Sequelize } = require('sequelize');
+const sequelize = new Sequelize("sqlite::memory:");
 
-// Uncomment the code below to use Mongoose ORM
-// const mongoose = require('mongoose');
+const Trade = sequelize.define('Trade', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [['buy', 'sell']]
+    }
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  symbol: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  shares: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1,
+      max: 100
+    }
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  timestamp: {
+    type: DataTypes.BIGINT,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'Trade',
+  timestamps: false,
+  indexes: [
+    {
+      unique: true,
+      fields: ['type', 'user_id']
+    }
+  ]
+});
 
+module.exports = Trade;
 
-// Insert your model definition below
