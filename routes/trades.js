882ff0cabf717c46415371423a699b04093afde3
Sergei <dev.sergeialimov@gmail.com>
2024-02-22 18:30:44 +0400

    feat: add trades routes

--- routes/trades.js
+++ routes/trades.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const tradesController = require('../controllers/trades');
+
+router.post('/', tradesController.createTrade);
+router.get('/', tradesController.getTrades);
+router.get('/:id', tradesController.getTradeById);
+router.put('/:id', tradesController.methodNotAllowed);
+router.patch('/:id', tradesController.methodNotAllowed);
+router.delete('/:id', tradesController.methodNotAllowed);
 
 module.exports = router;
