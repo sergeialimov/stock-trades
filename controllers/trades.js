a5960d0db92a9be018e42858ca66ab74fd86c70f
Sergei <dev.sergeialimov@gmail.com>
2024-02-23 13:10:17 +0400

    refactor: indentation

--- controllers/trades.js
+++ controllers/trades.js
@@ -1,47 +1,47 @@
 const Trade = require('../models/trades');
 
 exports.createTrade = async (req, res) => {
-  if (req.body.shares < 1 || req.body.shares > 100 || !['buy', 'sell'].includes(req.body.type)) {
-    return res.status(400).send('Invalid trade data');
-  }
+    if (req.body.shares < 1 || req.body.shares > 100 || !['buy', 'sell'].includes(req.body.type)) {
+        return res.status(400).send('Invalid trade data');
+    }
 
-  try {
-    const trade = new Trade(req.body);
-    await trade.save();
-    res.status(201).json(trade);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+    try {
+        const trade = new Trade(req.body);
+        await trade.save();
+        res.status(201).json(trade);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 };
 
 exports.getTrades = async (req, res) => {
-  const query = {};
-  if (req.query.type) query.type = req.query.type;
-  if (req.query.user_id) query.user_id = parseInt(req.query.user_id, 10);
+    const query = {};
+    if (req.query.type) query.type = req.query.type;
+    if (req.query.user_id) query.user_id = parseInt(req.query.user_id, 10);
 
-  try {
-    const trades = await Trade.findAll({
-      where: query,
-      order: [['id', 'ASC']]
-    });
-    res.status(200).json(trades);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+    try {
+        const trades = await Trade.findAll({
+            where: query,
+            order: [['id', 'ASC']]
+        });
+        res.status(200).json(trades);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 };
 
 exports.getTradeById = async (req, res) => {
-  try {
-    const trade = await Trade.findByPk(req.params.id);
-    if (!trade) {
-      return res.status(404).send('ID not found');
+    try {
+        const trade = await Trade.findByPk(req.params.id);
+        if (!trade) {
+            return res.status(404).send('ID not found');
+        }
+        res.status(200).json(trade);
+    } catch (error) {
+        res.status(500).send(error.message);
     }
-    res.status(200).json(trade);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
 };
 
 exports.methodNotAllowed = (req, res) => {
-  res.status(405).send('Method Not Allowed');
+    res.status(405).send('Method Not Allowed');
 };
--- models/trades.js
+++ models/trades.js
@@ -2,53 +2,52 @@ const { DataTypes, Sequelize } = require('sequelize');
 const sequelize = new Sequelize("sqlite::memory:");
 
 const Trade = sequelize.define('Trade', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  type: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      isIn: [['buy', 'sell']]
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isIn: [['buy', 'sell']]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    symbol: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    shares: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 1,
+            max: 100
+        }
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    timestamp: {
+        type: DataTypes.BIGINT,
+        allowNull: false
     }
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  symbol: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  shares: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    validate: {
-      min: 1,
-      max: 100
-    }
-  },
-  price: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  timestamp: {
-    type: DataTypes.BIGINT,
-    allowNull: false
-  }
 }, {
-  sequelize,
-  modelName: 'Trade',
-  timestamps: false,
-  indexes: [
-    {
-      unique: true,
-      fields: ['type', 'user_id']
-    }
-  ]
+    sequelize,
+    modelName: 'Trade',
+    timestamps: false,
+    indexes: [
+        {
+            unique: true,
+            fields: ['type', 'user_id']
+        }
+    ]
 });
 
 module.exports = Trade;
-
